5923b197394e811e282d262a056ce23bc744381c
Linda Wu <pinky510100@gmail.com>
2020-04-06 23:51:36 +0800

    add login verifier

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,15 @@ class ApplicationController < ActionController::Base
     rescue_from ActiveRecord::RecordNotFound, 
     with: :record_not_found
 
+    before_action :check_login
 
     private
+    def check_login
+        redirect_to login_path unless :current_user
+    end
+    def current_user
+        session[:ccc]
+    end
     def record_not_found
         render file: 'public/404.html',
              status: 404,
