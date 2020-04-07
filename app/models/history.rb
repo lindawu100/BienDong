833282f70046772785b0fa741ecdf810a340ca6b
Linda Wu <pinky510100@gmail.com>
2020-04-07 12:46:20 +0800

    add user helper

---
+++ app/models/history.rb
@@ -0,0 +1,4 @@
+class History < ApplicationRecord
+  belongs_to :event
+  belongs_to :user
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
 
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true, confirmation: true, length: { minimum: 2 }
+    
+    has_many :events
+    has_many :events, through: :histories
 end
